b19b635a77293674e551a11549b96610c722a972
adwobs <bota.adwoba@ashesi.edu.gh>
2017-02-07 12:39:05 +0000

    update on js and index

---
+++ scripts/index.js
@@ -0,0 +1,61 @@
+﻿(function () {
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+    var pictureSource;
+    var destinationType;
+
+    function onDeviceReady() {
+        pictureSource = navigator.camera.PictureSourceType;
+        destinationType = navigator.camera.DestinationType;
+
+//
+		$(function () {
+             "use strict";
+             $("#scanBarcode").click(function () {
+                 //alert("clicked");
+                 cordova.plugins.barcodeScanner.scan(
+                     function (result) {
+                         alert("Information: " + result.text + "\n" +
+                            "Format: " + result.format + "\n");
+                     },
+                     function (error) {
+                         alert(error);
+                     }
+                 );
+                 //end
+             });
+         });
+		//
+
+		//Geolocation start
+        /* var c = function (pos) {
+             var lat = pos.coords.latitude,
+                 long = pos.coords.longitude,
+                 coords = lat + ', ' + long;
+
+             document.getElementById('google-map').setAttribute('src', 'http://maps.google.co.uk?q=' + coords + '&z=60&output=embed');
+
+         }
+
+         //get location function
+         document.getElementById("getloc").onclick = function () {
+             navigator.geolocation.getCurrentPosition(c);
+             return false;
+         }
+
+    };
+
+    function onPhotoDataSuccess(imageData) {
+
+    }
+
+    function onFail(message) {
+
+    }
+
+    function clearCache() {
+
+    }  */
+
+})();
+
